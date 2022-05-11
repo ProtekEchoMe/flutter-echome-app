982d996dbb0cd63ce31af474545bd7aaeae3ca1c
Mixson <mixsonit@gmail.com>
2022-05-11 16:03:56 +0800

    added asset return button to home page

--- lib/pages/home/route_constant.dart
+++ lib/pages/home/route_constant.dart
@@ -7,8 +7,10 @@ class RouteConstant {
     RouteObject("Asset Inventory", "Check you inventory", "/asset_inventory", MdiIcons.warehouse),
     RouteObject("Transfer In", "Receive from other site", "/transfer_in", MdiIcons.inboxArrowDown),
     RouteObject("Transfer Out", "Send to other site", "/transfer_out",  MdiIcons.inboxArrowUp),
+    RouteObject("Asset Return", "Return Inventory", "/transfer_out",  MdiIcons.messageArrowLeft),
     RouteObject("Assets Disposal", "Disposal and write-off", "/asset_disposal", MdiIcons.trashCan,),
     RouteObject("Stock Take", "Count your asset", "/stock_take", MdiIcons.fileDocumentMultipleOutline),
+
     // RouteObject("Senser Setting", "For Debug", "/debug", MdiIcons.deathStar)
   ];
 }
