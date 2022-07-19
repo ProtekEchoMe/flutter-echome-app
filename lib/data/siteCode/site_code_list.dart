ad329f6bf81faa97ec30c5aea3b91ab68f736da2
Mixson <mixsonit@gmail.com>
2022-07-19 10:55:01 +0800

    test

--- lib/data/siteCode/site_code_list.dart
+++ lib/data/siteCode/site_code_list.dart
@@ -1,5 +1,5 @@
 class SiteCodeList {
   static List<String> getList(){
-    return ["1", "87", "HKG_ATL_3F", "HKG_HTC_2F","HKG_MK_3F", "9354"];
+    return ["1", "87", "HKG_ATL_3F", "HKG_HTC_2F","HKG_MK_3F", "9354", "2"];
   }
 }
